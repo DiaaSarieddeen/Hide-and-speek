0ae622e5b1e1b19e730ac897d88d923fd214cf2f
BilalAbouBaraki <bilalaboub1452001@gmail.com>
2023-09-21 19:17:48 +0300

    message chat and search and profile

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { SearchBar } from "./SearchBar";
 import { Homepage } from "./Homepage";
-import {Profile} from "./Profile";
+import { Profile } from "./Profile";
+import './Chat.css'
 
 export const Chat = () => (
-  <div>
-    <Profile/>
-    <Homepage/>
+  <div className="chat-container">
+    <div className="profile-container">
+      <Profile />
+    </div>
+    <div className="homepage-container">
+      <Homepage />
+    </div>
   </div>
 );
+
 /*
  <h1>{localStorage.getItem("name")}</h1>
     <h1>{localStorage.getItem("email")}</h1>
